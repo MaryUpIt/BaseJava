ea69b7b90d03c94353e21720194a14ad4bfa6237
MaryUp <maryup.for.reg@gmail.com>
2022-10-21 17:58:11 +0300

    HW#3 class ArrayStorage -> extends AbstractArrayStorage

--- src/com/urise/webapp/storage/ArrayStorage.java
+++ src/com/urise/webapp/storage/ArrayStorage.java
@@ -4,37 +4,24 @@ import com.urise.webapp.model.Resume;
 
 import java.util.Arrays;
 
-/**
- * Array based storage for Resumes
- */
-public class ArrayStorage {
-    private Resume[] storage = new Resume[10000];
-    private int size;
+public class ArrayStorage extends AbstractArrayStorage {
 
-    public void clear() {
-        Arrays.fill(storage, 0, size, null);
-        size = 0;
-        System.out.println("Хранилище очищено");
-    }
 
+
+
+    @Override
     public void save(Resume resume) {
-        if (findIndex(resume.getUuid()) != -1) {
+        if (size >= STORAGE_LIMIT) {
+            System.out.println("Хранилище переполнено");
+        } else if (findIndex(resume.getUuid()) != -1) {
             System.out.println("Такое резюме уже есть в хранилище,  воспользуйтесь методом update");
-            return;
-        }
-        if (resume == null) {
-            System.out.println("Введите uuid резюме");
-            return;
-        }
-        if (size < storage.length) {
+        } else {
             storage[size++] = resume;
             System.out.println("Добавлено резюме: " + resume);
-
-        } else {
-            System.out.println("Хранилище переполнено");
         }
     }
 
+    @Override
     public void update(Resume resume) {
         int index = findIndex(resume.getUuid());
         if (index == -1) {
@@ -46,15 +33,7 @@ public class ArrayStorage {
 
     }
 
-    public Resume get(String uuid) {
-        int index = findIndex(uuid);
-        if (index != -1) {
-            return storage[index];
-        }
-        System.out.printf("резюме с идентификатором %s отсутствует в хранилище.\n", uuid);
-        return null;
-    }
-
+    @Override
     public void delete(String uuid) {
         int index = findIndex(uuid);
         if (index != -1) {
@@ -67,21 +46,26 @@ public class ArrayStorage {
 
     }
 
+
+
+    @Override
+    public void clear() {
+        Arrays.fill(storage, 0, size, null);
+        size = 0;
+        System.out.println("Хранилище очищено");
+    }
+
+    @Override
     public Resume[] getAll() {
         return Arrays.copyOf(storage, size);
     }
 
-    public int size() {
-        return size;
-    }
-
-    private int findIndex(String uuid) {
-        int index = 0;
-        for (Resume resume : getAll()) {
-            if (resume.getUuid().equals(uuid)) {
-                return index;
+    protected int findIndex(String uuid) {
+        Resume resume = new Resume(uuid);
+        for (int i = 0; i < size; i++) {
+            if (storage[i].equals(resume)) {
+                return i;
             }
-            index++;
         }
         return -1;
     }
