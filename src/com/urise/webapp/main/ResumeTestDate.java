2d97ef0272e22ce34bc230b58114216c14a5f02c
MaryUp <maryup.for.reg@gmail.com>
2022-11-28 12:38:31 +0300

    HW#8 Create types

--- src/com/urise/webapp/main/ResumeTestDate.java
+++ src/com/urise/webapp/main/ResumeTestDate.java
@@ -15,9 +15,9 @@ public class ResumeTestDate {
         resume.setContacts(PHONE, "+7-926-567-34-74");
         resume.setContacts(GITHUB, "github.com");
         resume.setContacts(LINKEDIN, "LinkedIn");
-        resume.setContacts(EMAIL,"email@gmail.com");
+        resume.setContacts(EMAIL, "email@gmail.com");
         resume.setSections(PERSONAL, new TextSection("analytical mindset, logic, creativity, initiative"));
-        List<String>qualifications = new ArrayList<>();
+        List<String> qualifications = new ArrayList<>();
         qualifications.add("Languages:Java, JavaScript, Python, SQL.");
         qualifications.add("Version control: Git.");
         qualifications.add("DB: PostgreSQL.");
@@ -26,18 +26,17 @@ public class ResumeTestDate {
         resume.setSections(QUALIFICATIONS, new ListSection(qualifications));
 
         Map<Period, Organization> experience = new HashMap<>();
-        experience.put(new Period(LocalDate.of(2017,1,1),LocalDate.of(2020,1,1)),
+        experience.put(new Period(LocalDate.of(2017, 1, 1), LocalDate.of(2020, 1, 1)),
                 new Organization("Yandex", "Java Developer"));
-        experience.put(new Period(LocalDate.of(2015,1,1),LocalDate.of(2017,1,1)),
+        experience.put(new Period(LocalDate.of(2015, 1, 1), LocalDate.of(2017, 1, 1)),
                 new Organization("Microsoft", "Junior Java Trainee"));
         resume.setSections(EXPERIENCE, new OrganizationSections(experience));
 
         System.out.println(resume);
         System.out.println(PHONE.getTitle() + " :" + resume.getContacts(PHONE));
-        System.out.println(GITHUB.getTitle() + " : "+ resume.getContacts(GITHUB));
+        System.out.println(GITHUB.getTitle() + " : " + resume.getContacts(GITHUB));
         System.out.println(QUALIFICATIONS.getTitle() + "\n" + resume.getSection(QUALIFICATIONS));
         System.out.println(EXPERIENCE.getTitle() + "\n" + resume.getSection(EXPERIENCE));
-
 
 
     }
