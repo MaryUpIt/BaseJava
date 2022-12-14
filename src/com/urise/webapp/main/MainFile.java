c41cede47aa934d853d4a985fa8eb9137daf0b4d
MaryUp <maryup.for.reg@gmail.com>
2022-12-14 19:52:42 +0300

    HW#09 printFilesTree + offset

--- src/com/urise/webapp/main/MainFile.java
+++ src/com/urise/webapp/main/MainFile.java
@@ -8,13 +8,13 @@ import java.nio.file.Paths;
 
 public class MainFile {
     public static void main(String[] args) throws IOException {
-        String pathName = "..\\basejava\\src\\com\\urise\\webapp";
+        String pathName = "..\\basejava\\src\\com\\urise";
         //  pathName = "..\\basejava\\resumes";
         File fileDirectory = new File(pathName);
         System.out.println(fileDirectory.getCanonicalFile());
         System.out.println(fileDirectory.getName());
         System.out.println(fileDirectory.isDirectory());
-        printFilesTree(fileDirectory);
+        printFilesTree(fileDirectory, "");
         Path pathDirectory = Paths.get(pathName);
         System.out.println(pathDirectory.getFileName());
         System.out.println(pathDirectory.toAbsolutePath().getRoot());
@@ -24,15 +24,15 @@ public class MainFile {
 
     }
 
-    private static void printFilesTree(File directory) {
-        System.out.println("Directory: " + directory.getName());
+    private static void printFilesTree(File directory, String offset) {
+        System.out.println(offset +"Directory: " + directory.getName());
         File[] files = directory.listFiles();
         if (files != null) {
             for (File file : files) {
                 if (file.isDirectory()) {
-                    printFilesTree(file);
+                    printFilesTree(file, offset + "  ");
                 } else {
-                    System.out.println("-> file: " + file.getName());
+                    System.out.println(offset + "  -> file: " + file.getName());
                 }
             }
         }
