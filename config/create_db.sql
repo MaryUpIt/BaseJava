bec5cd39ddc664692dbd31f8da68473894a6428c
MaryUp <maryup.for.reg@gmail.com>
2023-02-21 20:05:42 +0300

    hw13

--- config/create_db.sql
+++ config/create_db.sql
@@ -13,17 +13,21 @@ CREATE TABLE contacts
     id          SERIAL PRIMARY KEY,
     type        TEXT     NOT NULL,
     value       TEXT     NOT NULL,
-    resume_uuid CHAR(36) NOT NULL CONSTRAINT contacts_resume_uuid_fk
-        REFERENCES resume ON UPDATE RESTRICT ON DELETE CASCADE
+    resume_uuid CHAR(36) NOT NULL
+        CONSTRAINT contacts_resume_uuid_fk
+            REFERENCES resume ON UPDATE RESTRICT ON DELETE CASCADE
 
 );
 
 
 CREATE TABLE sections
 (
-    id   SERIAL PRIMARY KEY,
-    type TEXT NOT NULL,
-    value
+    id          SERIAL PRIMARY KEY,
+    type        TEXT     NOT NULL,
+
+    resume_uuid CHAR(36) NOT NULL
+        CONSTRAINT contacts_resume_uuid_fk
+            REFERENCES resume ON UPDATE RESTRICT ON DELETE CASCADE
 );
 
 
