034bbe5742e2d5145df59e4803bd3f500cb2b494
MaryUp <maryup.for.reg@gmail.com>
2023-02-24 23:29:39 +0300

    hw13 Lesson13 create SqlAdapter

--- src/com/urise/webapp/storage/SqlStorage.java
+++ src/com/urise/webapp/storage/SqlStorage.java
@@ -1,122 +1,97 @@
 package com.urise.webapp.storage;
 
-import com.urise.webapp.exceptions.ExistStorageException;
 import com.urise.webapp.exceptions.NotExistStorageException;
 import com.urise.webapp.exceptions.StorageException;
 import com.urise.webapp.model.Resume;
-import com.urise.webapp.sql.ConnectionFactory;
+import com.urise.webapp.sql.SqlAdapter;
 import com.urise.webapp.util.Config;
 
-import java.sql.*;
-import java.util.ArrayList;
+import java.sql.ResultSet;
 import java.util.Comparator;
+import java.util.LinkedList;
 import java.util.List;
 
 public class SqlStorage implements Storage {
-    public final ConnectionFactory connectionFactory;
+    public final SqlAdapter sqlAdapter;
     private static final String URL = Config.getInstance().getUrl();
     private static final String USER = Config.getInstance().getUser();
     private static final String PASSWORD = Config.getInstance().getPassword();
     private static final Comparator<Resume> NAME_COMPARATOR = Comparator.comparing(Resume::getFullName).thenComparing(Resume::getUuid);
 
     public SqlStorage() {
-        this.connectionFactory = () -> DriverManager.getConnection(URL, USER, PASSWORD);
+        this.sqlAdapter = new SqlAdapter(URL, USER, PASSWORD);
     }
 
     @Override
     public int size() {
-        try (Connection connection = connectionFactory.getConnection();
-             PreparedStatement prepared = connection.prepareStatement("SELECT count(*) size FROM  resume")) {
-            ResultSet result = prepared.executeQuery();
+        return sqlAdapter.execute("SELECT count(*) size FROM  resume", statement -> {
+            ResultSet result = statement.executeQuery();
             if (!result.next()) {
                 throw new StorageException("Storage is empty");
             }
             return result.getInt("size");
-        } catch (SQLException e) {
-            throw new StorageException(e);
-        }
-
+        });
     }
-
 
     @Override
     public void save(Resume resume) {
-        try (Connection connection = connectionFactory.getConnection();
-             PreparedStatement prepared = connection.prepareStatement("INSERT INTO  resume (uuid, full_name)VALUES (?,?)")) {
-            prepared.setString(1, resume.getUuid());
-            prepared.setString(2, resume.getFullName());
-            prepared.execute();
-        } catch (SQLException e) {
-            throw new ExistStorageException(e.getMessage());
-        }
+        sqlAdapter.execute("INSERT INTO  resume (uuid, full_name)VALUES (?,?)", statement -> {
+            statement.setString(1, resume.getUuid());
+            statement.setString(2, resume.getFullName());
+            statement.execute();
+        });
     }
 
     @Override
     public void update(Resume resume) {
-        get(resume.getUuid());
-        try (Connection connection = connectionFactory.getConnection();
-             PreparedStatement prepared = connection.prepareStatement("UPDATE resume  SET full_name= ? WHERE uuid=?")) {
-            prepared.setString(2, resume.getUuid());
-            prepared.setString(1, resume.getFullName());
-            prepared.execute();
-        } catch (SQLException e) {
-            throw new StorageException(e);
-        }
-
+        sqlAdapter.execute("UPDATE resume  SET full_name= ? WHERE uuid=?", statement -> {
+            statement.setString(2, resume.getUuid());
+            statement.setString(1, resume.getFullName());
+            if (statement.executeUpdate() == 0) {
+                throw new NotExistStorageException(resume.getUuid());
+            }
+        });
     }
 
     @Override
     public void delete(String uuid) {
-        get(uuid);
-        try (Connection connection = connectionFactory.getConnection();
-             PreparedStatement prepared = connection.prepareStatement("DELETE FROM resume r WHERE r.uuid =?")) {
-            prepared.setString(1, uuid);
-            prepared.execute();
-
-        } catch (SQLException e) {
-            throw new StorageException(e);
-        }
+        sqlAdapter.execute("DELETE FROM resume r WHERE r.uuid =?", statement -> {
+            statement.setString(1, uuid);
+            if (statement.executeUpdate() == 0) {
+                throw new NotExistStorageException(uuid);
+            }
+        });
     }
 
     @Override
     public Resume get(String uuid) {
-        try (Connection connection = connectionFactory.getConnection();
-             PreparedStatement prepared = connection.prepareStatement("SELECT *FROM  resume r WHERE r.uuid =?")) {
-            prepared.setString(1, uuid);
-            ResultSet result = prepared.executeQuery();
+        return sqlAdapter.execute("SELECT *FROM  resume r WHERE r.uuid =?", statement -> {
+            statement.setString(1, uuid);
+            ResultSet result = statement.executeQuery();
             if (!result.next()) {
                 throw new NotExistStorageException(uuid);
             }
             return new Resume(uuid, result.getString("full_name"));
-        } catch (SQLException e) {
-            throw new StorageException(e);
-        }
+        });
     }
 
     @Override
     public void clear() {
-        try (Connection connection = connectionFactory.getConnection();
-             PreparedStatement prepared = connection.prepareStatement("DELETE FROM resume")) {
-            prepared.execute();
-        } catch (SQLException e) {
-            throw new StorageException(e);
-        }
+        sqlAdapter.execute("DELETE FROM resume", statement -> {
+            statement.execute();
+        });
     }
 
     @Override
     public List<Resume> getAllSorted() {
-        List<Resume> resumes = new ArrayList<>();
-        try (Connection connection = connectionFactory.getConnection();
-             PreparedStatement prepared = connection.prepareStatement("SELECT *FROM  resume")) {
-            ResultSet result = prepared.executeQuery();
+        List<Resume> resumes = new LinkedList<>();
+        return sqlAdapter.execute("SELECT *FROM  resume", statement -> {
+            ResultSet result = statement.executeQuery();
             while (result.next()) {
                 resumes.add(new Resume(result.getString("uuid"), result.getString("full_name")));
             }
             resumes.sort(NAME_COMPARATOR);
             return resumes;
-        } catch (SQLException e) {
-            throw new StorageException(e);
-        }
+        });
     }
-
 }
